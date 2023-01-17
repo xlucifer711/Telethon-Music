0cd38c07099a38bb88bcfddc3fd502f0eb6c5050
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2023-01-17 12:17:43 +0200

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("APP_ID", "6435225"))
+    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN"," ")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
+    HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "z_09bot")
+    SUPPORT = os.environ.get("SUPPORT", "SORS0Coo") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "c_r_source") # Your Channel
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/54699e9f531dfac087926.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/9fd0a28073d92e3052fe3.jpg")
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
+    AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
+    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
