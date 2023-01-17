df2c8be9920a5876621feed90ce2cd73fd5fc0ee
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2023-01-17 21:01:50 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN"," ")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BJWap1wBu5cV_ZdrPpJWQY2WczEEAXMjgXIxEscBKD9ap1duWFCbHM8xtbFoyHfdAQ7-h2lBFC3bNmuoBuB2aIY7hIG_hvMyXlUBCbCxPnLn4tKhGqC9liKmWE7Vqu2NMasosSdeLCnqNp6BIzxpoIdvl1ORJOaAj5hrAulng4EfxS9ekKp6hCuMsbwC0rsADYlAAMvwCs7OmQyDb-9uDhAvV3yRnb3BbV44GmQImHIDpU-8zZQIi4AJvA6OBaJ2kZVh4RSoaVy7T9jwHRUPTlcRF_RBnE4XJlR2a2-QLEEeP8GpLl81kwe2l2h_pW24kFX_O57NAqRXsDJR6lslXLHXZDdaDDk=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
