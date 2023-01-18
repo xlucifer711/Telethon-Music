15a2ccb6670001ca8dfb1027d46b90564fd22ce7
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2023-01-18 13:29:53 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config(object):
     API_ID = int(os.environ.get("APP_ID", "6435225"))
     API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN"," ")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "1BJWap1wBu7xoXyuRKNczObWv8TDqf5n1CL8jk71hAewDUPYRADHB3j_rgzd5E0OGVNW65yrH3yCTIhU06baXfC4bPkpzk4UlKBVP36cpM4VJwMU6zraNVQ9YkAEPJAhgqaH1pE6yjtPHGYq4UD6kyXGeFEs54sFGc3vsaL7nKMJ_i10NTmwqZX3uwQrY7adEAQTNQMLTedyudq6YnMe_Wli9N4mV1LPPQB4c5L537O0z2IqLNwWYcgTNJ6KUrm0aLZ814UzbxuTc_8XQgtl99ckxaiLpq8rYAcjuyuN7dLpQpj2cV0ib965Fa-oX4Qp4StPC389sgPOAApOxjYH2uW31Pi5nHAU=") 
+    STRING_SESSION = os.environ.get("STRING_SESSION", "1BJWap1wBuyJ19lH68e8nw-eAn5KyqRJbKjJc2RIqkCRdvYk40vK3N-flpTpNqRPnJ2UMTnV0uCDZ9C5zWeqPx1PxV3GN_NDSAfXBCHtZfxUr6dWZaue3kTnOmNUy8tiG56Omr2xY6Lm2v014pUb8lL_0BhkQFq9BmEmfBThyucR3rY1WYAihFGH89bL85bJU1o-AUz2wA1038C9k0Qiisox9Yy0zNPo2sTc_eNdpon-AlDx392lFqC_nW5eNeG7zFHL9RiUuM0HNM2bJe4mWXbU2t1iVZbGT3DOWHpRAVBJtID5liOxRLJYfJ7LZoQz3r9vjw2O7z4VhuDi510VVVeWJx7bQKb4=") 
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
@@ -12,6 +12,6 @@ class Config(object):
     CHANNEL = os.environ.get("CHANNEL", "c_r_source") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/54699e9f531dfac087926.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/9fd0a28073d92e3052fe3.jpg")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5636767152")) # telegram I'd not Username
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "5701016541")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
